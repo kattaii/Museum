2a83a72406d67b027bae4671fca3096f2656b19d
dmakaridze <dmakaridze@gmail.com>
2020-07-21 16:56:15 +0400

    Add project folders

--- js/hover.js
+++ js/hover.js
@@ -14,74 +14,106 @@ let layers = [];
 let lid = 0;
 let st = true;
 
-$.get("data.json", function (data) {
-    for (let i = 0; i < data.length; i++) {
-        let layerId = -1;
-        for (let j = 0; j < layers.length; j++) {
-            if (layers[j].src === data[i].src) {
-                layerId = j;
-            }
-        }
-        if (layerId > -1) {
-            layers[layerId].points.push({
-                x: data[i].x * sc / 1.6765625,
-                y: data[i].y * sc / 1.6765625,
-                r: data[i].r * sc,
-            });
-        } else {
-            layers.push({
-                src: data[i].src,
-                text: data[i].text,
-                question: data[i].question,
-                points: [{
-                    x: data[i].x * sc / 1.6765625,
-                    y: data[i].y * sc / 1.6765625,
-                    r: data[i].r * sc,
-                }]
-            });
-        }
-    }
-    console.log(layers);
-    image.style.backgroundImage = 'url(' + layers[lid].src + ')';
-    infoBox.innerHTML = document.getElementById(layers[lid].question).innerHTML;
-    magnifier.style.backgroundImage = 'url(' + layers[lid + 1].src + ')';
+$(".select-project .dropdown-item").click(function () {
+    $(".select-project button").html($(this).html());
 
-    image.addEventListener('mousemove', function (ev) {
-        let bcr = image.getBoundingClientRect();
-        magnifier.style.top = ev.y - bcr.top - 39 + 'px';
-        magnifier.style.left = ev.x - bcr.left - 39 + 'px';
-        magnifier.style.backgroundPositionX = -ev.x + 38 + bcr.left + 'px';
-        magnifier.style.backgroundPositionY = -ev.y + 38 + bcr.top + 'px';
-    });
+    layers = [];
+    lid = 0;
+    st = true;
+    image.style.backgroundImage = "";
+    infoBox.innerHTML = "";
+    magnifier.style.backgroundImage = "";
+    $("#project-title").html("");
+    $("#templates").html("");
+    let btn = this;
+    let urlPref = "projects/" + btn.dataset.museumProject + "/";
 
-    image.addEventListener('click', function (ev) {
-        if (st) {
-            let bcr = image.getBoundingClientRect();
-            let points = layers[lid].points;
-            for (let p = 0; p < points.length; p++) {
-                let r = points[p].r;
-                if (Math.abs(ev.x - bcr.left - points[p].x) < r && Math.abs(ev.y - bcr.top - points[p].y) < r && lid < layers.length - 1) {
-                    infoBox.innerHTML = document.getElementById(layers[lid].text).innerHTML;
-                    lid = lid + 1;
-                    image.style.backgroundImage = 'url(' + layers[lid].src + ')';
-                    if (lid < layers.length - 1) {
-                        magnifier.style.backgroundImage = 'url(' + layers[lid + 1].src + ')';
-                        nextBtn.classList.remove("disabled");
+    $.get(urlPref + "templates.html", function (data) {
+        $("#templates").html(data);
+        $.get(urlPref + "data.json", function (data) {
+            $("#project-title").html(data.title);
+            let pnts = data.points;
+            for (let i = 0; i < pnts.length; i++) {
+                let layerId = -1;
+                for (let j = 0; j < layers.length; j++) {
+                    if (layers[j].src === pnts[i].src) {
+                        layerId = j;
                     }
-                    st = false;
-                    magnifier.style.display = 'none';
-                    return;
+                }
+                if (layerId > -1) {
+                    layers[layerId].points.push({
+                        x: pnts[i].x * sc,
+                        y: pnts[i].y * sc,
+                        r: pnts[i].r * sc,
+                    });
+                } else {
+                    layers.push({
+                        src: pnts[i].src,
+                        text: pnts[i].text,
+                        question: pnts[i].question,
+                        points: [{
+                            x: pnts[i].x * sc,
+                            y: pnts[i].y * sc,
+                            r: pnts[i].r * sc,
+                        }]
+                    });
                 }
             }
-        }
+            image.style.backgroundImage = 'url(' + urlPref + layers[lid].src + ')';
+            let tmpl = document.getElementById(layers[lid].question);
+            if (tmpl) {
+                infoBox.innerHTML = tmpl.innerHTML;
+            }
+            magnifier.style.backgroundImage = 'url(' + urlPref + layers[lid + 1].src + ')';
+
+            image.addEventListener('mousemove', function (ev) {
+                let bcr = image.getBoundingClientRect();
+                magnifier.style.top = ev.y - bcr.top - 39 + 'px';
+                magnifier.style.left = ev.x - bcr.left - 39 + 'px';
+                magnifier.style.backgroundPositionX = -ev.x + 38 + bcr.left + 'px';
+                magnifier.style.backgroundPositionY = -ev.y + 38 + bcr.top + 'px';
+            });
+
+            image.addEventListener('click', function (ev) {
+                if (st) {
+                    let bcr = image.getBoundingClientRect();
+                    let points = layers[lid].points;
+                    for (let p = 0; p < points.length; p++) {
+                        let r = points[p].r;
+                        if (Math.abs(ev.x - bcr.left - points[p].x) < r && Math.abs(ev.y - bcr.top - points[p].y) < r && lid < layers.length - 1) {
+                            let tmpl = document.getElementById(layers[lid].text);
+                            if (tmpl) {
+                                infoBox.innerHTML = tmpl.innerHTML;
+                            }
+                            lid = lid + 1;
+                            image.style.backgroundImage = 'url(' + urlPref + layers[lid].src + ')';
+                            if (lid < layers.length - 1) {
+                                magnifier.style.backgroundImage = 'url(' + urlPref + layers[lid + 1].src + ')';
+                                nextBtn.classList.remove("disabled");
+                            }
+                            st = false;
+                            magnifier.style.display = 'none';
+                            return;
+                        }
+                    }
+                }
+            });
+
+            nextBtn.addEventListener('click', function () {
+                if (!this.classList.contains("disabled")) {
+                    let tmpl = document.getElementById(layers[lid].question);
+                    if (tmpl) {
+                        infoBox.innerHTML = tmpl.innerHTML;
+                    }
+                    magnifier.style.display = 'block';
+                    st = true;
+                    this.classList.add("disabled");
+                }
+            });
+        });
     });
 
-    nextBtn.addEventListener('click', function () {
-        if (!this.classList.contains("disabled")) {
-            infoBox.innerHTML = document.getElementById(layers[lid].question).innerHTML;
-            magnifier.style.display = 'block';
-            st = true;
-            this.classList.add("disabled");
-        }
-    });
+
 });
+
+
